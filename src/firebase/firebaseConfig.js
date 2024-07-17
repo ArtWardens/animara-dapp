d0f42755a1036b6d8148742b580a54abf72d986b
Jack Yeoh <jackyeoh0808@gmail.com>
2024-07-17 14:26:11 +0800

    fix: missing firebase functions libs

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,6 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getFunctions, httpsCallable } from "firebase/functions";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOJl7jgY2SMFGsywAi1NGWcBdPl5FPBqQ",
