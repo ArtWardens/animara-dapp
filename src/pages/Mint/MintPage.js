27d3aa653ca6b61903568017422cdcd1650b5fd9
y3jiojio <triple.y.yvetteyee@gmail.com>
2024-08-12 19:40:17 +0800

    chore: make DAPP Layout Stable

--- src/pages/Mint/MintPage.js
+++ src/pages/Mint/MintPage.js
@@ -36,7 +36,7 @@ function ReferralPage({ currentUser, totalClicks }) {
             <Header currentUser={currentUser} totalClicks={totalClicks} />
 
             <div
-                className="flex flex-col items-center pb-4 px-20 min-h-screen"
+                className="flex flex-col items-center pb-8 px-20 min-h-screen"
                 style={{
                     backgroundImage: 'url("../../assets/images/clicker-character/clickerWall.png")',
                     backgroundSize: 'cover',
@@ -45,11 +45,11 @@ function ReferralPage({ currentUser, totalClicks }) {
                     backgroundAttachment: 'fixed',
                 }}
             >
-                <div className="grid grid-cols-5 w-full gap-16 container pt-36 tracking-wider items-center">
+                <div className="grid grid-cols-5 w-full gap-16 container pt-40 tracking-wider items-center">
                     <div className="col-span-2 text-amber-500 grid gap-8">
                         <div className="py-4">
                             <span
-                                className="text-6xl"
+                                className="text-5xl"
                                 style={{
                                     WebkitTextStrokeWidth: "3px",
                                     WebkitTextStrokeColor: "var(--Color-11, #FFF)",
@@ -59,7 +59,7 @@ function ReferralPage({ currentUser, totalClicks }) {
                             </span>
                             <br />
                             <span
-                                className="text-8xl"
+                                className="text-7xl"
                                 style={{
                                     WebkitTextStrokeWidth: "4px",
                                     WebkitTextStrokeColor: "var(--Color-11, #FFF)",
@@ -79,7 +79,7 @@ function ReferralPage({ currentUser, totalClicks }) {
                             <p
                                 className="leading-normal tracking-normal text-white font-outfit text-justify"
                                 style={{
-                                    fontSize: '16px',
+                                    fontSize: '14px',
                                 }}
                             >
                                 Complete the following tasks to get a 25% whitelist discount!
@@ -96,7 +96,7 @@ function ReferralPage({ currentUser, totalClicks }) {
                             <p
                                 className="leading-normal tracking-normal text-white font-outfit text-justify"
                                 style={{
-                                    fontSize: '16px',
+                                    fontSize: '14px',
                                 }}
                             >
                                 Mint your NFTs and unlock your Anitap VIP Value Pass today!
@@ -113,14 +113,14 @@ function ReferralPage({ currentUser, totalClicks }) {
                             <p
                                 className="leading-normal tracking-normal text-white font-outfit text-justify"
                                 style={{
-                                    fontSize: '16px',
+                                    fontSize: '14px',
                                 }}
                             >
                                 Use your VIP Pass to join the Animara leaderboard event and compete to win prizes worth up to $1,000,000!
                             </p>
                         </div>
 
-                        <div className="border-2 border-transparent border-t-sky-300 border-dashed py-8 font-outfit text-lg">
+                        <div className="border-2 border-transparent border-t-sky-300 border-dashed py-8 mt-4 font-outfit text-md">
                             <span className="cursor-pointer text-amber-500 hover:text-amber-400">
                                 <a href="https://opensea.io/login" target="_blank" rel="noopener noreferrer">
                                     Opensea Link
@@ -132,12 +132,12 @@ function ReferralPage({ currentUser, totalClicks }) {
                                     Etherscan
                                 </a>
                             </span>
-                            <p className="text-white text-sm">You must be on the Poleygon Mainnet to mint.</p>
+                            <p className="text-white text-xs">You must be on the Poleygon Mainnet to mint.</p>
                         </div>
                     </div>
 
                     <div
-                        className="col-span-3 rounded-3xl p-3"
+                        className="col-span-3 rounded-3xl p-3 "
                         style={{
                             border: '2px solid var(--Color, #F4FBFF)',
                             background: 'rgba(155, 231, 255, 0.58)',
@@ -164,7 +164,7 @@ function ReferralPage({ currentUser, totalClicks }) {
                         </div>
 
                         <div
-                            className="rounded-2xl place-content-center p-8 grid"
+                            className="rounded-2xl place-content-center p-6 grid min-h-[60vh] lg:min-h-[80dvh] 2xl:min-h-[50dvh]"
                             style={{
                                 backgroundImage: 'url("../assets/images/clicker-character/mintBBG.png")',
                                 backgroundSize: 'cover',
@@ -173,7 +173,7 @@ function ReferralPage({ currentUser, totalClicks }) {
                             }}
                         >
                             {isContainerVisible && (
-                                <div className="p-4 px-6 bg-[#003260] rounded-3xl shadow-inner border border-[#7fc1ff] flex-col justify-self-center items-center gap-2 inline-flex">
+                                <div className="p-3 px-6 bg-[#003260] rounded-3xl shadow-inner border border-[#7fc1ff] flex-col justify-self-center items-center gap-2 inline-flex">
                                     <p className="text-md pb-1">Missions Ends In</p>
                                     <div className="h-[50px] justify-start items-center inline-flex gap-1 pb-3">
                                         <div className="w-12 h-1/2 bg-[#003260] shadow-inner flex-col justify-center items-center gap-1 inline-flex">
@@ -207,21 +207,21 @@ function ReferralPage({ currentUser, totalClicks }) {
                                 </div>
                             )}
 
-                            <img src="../assets/images/clicker-character/nft-treasureBox.png" alt="NFT Treasure Box" className="object-cover w-full -my-10"/>
+                            <img src="../assets/images/clicker-character/nft-treasureBox.png" alt="NFT Treasure Box" className="object-contain w-96 -my-10"/>
 
                             <div className="w-full flex-col justify-center items-center inline-flex">
                                 <div className="flex-col justify-center items-center flex pb-8">
                                     <span className="text-gray-200 text-xl line-through">
                                         0.097 SOL
                                     </span>
-                                    <div className="text-center absolute right-36 bottom-56 text-white text-sm border-4 px-3 py-1 border-white rounded-2xl bg-sky-500 rotate-[20deg]">
+                                    <div className="text-center absolute right-36 bottom-52 text-white text-sm border-4 px-3 py-1 border-white rounded-2xl bg-sky-500 rotate-[20deg]">
                                         Early Bird<br />
                                         Bonus Active!
                                     </div>
                                     <div
                                         className="text-center text-amber-500 text-6xl z-10"
                                         style={{
-                                            WebkitTextStrokeWidth: '4px',
+                                            WebkitTextStrokeWidth: '3.5px',
                                             WebkitTextStrokeColor: 'var(--COlor-11, #FFF)',
                                             textShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
                                         }}
@@ -232,9 +232,9 @@ function ReferralPage({ currentUser, totalClicks }) {
                             </div>
 
                             <div className="justify-center items-center inline-flex hover:scale-105 transition-transform duration-200">
-                                <div className="h-[100px] w-[300px] bg-[#FFDC62] rounded-full border border-[#E59E69] justify-center items-center inline-flex shadow-[0px_4px_4px_0px_#FFFBEF_inset,0px_-4px_4px_0px_rgba(255,249,228,0.48),0px_5px_4px_0px_rgba(232,140,72,0.48)] hover:bg-[#FFB23F] hover:pl-[24px] hover:pr-[20px] hover:border-1 hover:border-[#E59E69] hover:shadow-[0px_4px_4px_0px_rgba(255,210,143,0.61)_inset,0px_4px_4px_0px_rgba(136,136,136,0.48)] cursor-pointer">
+                                <div className="h-[90px] w-[270px] bg-[#FFDC62] rounded-full border border-[#E59E69] justify-center items-center inline-flex shadow-[0px_4px_4px_0px_#FFFBEF_inset,0px_-4px_4px_0px_rgba(255,249,228,0.48),0px_5px_4px_0px_rgba(232,140,72,0.48)] hover:bg-[#FFB23F] hover:pl-[24px] hover:pr-[20px] hover:border-1 hover:border-[#E59E69] hover:shadow-[0px_4px_4px_0px_rgba(255,210,143,0.61)_inset,0px_4px_4px_0px_rgba(136,136,136,0.48)] cursor-pointer">
                                     <div
-                                        className="text-center text-white text-4xl font-normal"
+                                        className="text-center text-white text-3xl font-normal"
                                         style={{
                                             textShadow: '0px 2px 0.6px rgba(240, 139, 0, 0.66)'
                                         }}
