e0579935c102153353e1f8bcd11e59d637ba0858
TanHouYin <155421724+TanHouYin@users.noreply.github.com>
2024-08-21 11:01:33 +0800

    chore: update limited access & email verification page design

--- src/pages/VerifyEmail/LimitedAccessPage.js
+++ src/pages/VerifyEmail/LimitedAccessPage.js
@@ -28,20 +28,20 @@ const LimitedAccessPage = () => {
 
       {/* Content */}
       <div
-        className="w-auto h-[70dvh] items-center justify-center bg-contain bg-no-repeat"
+        className="w-auto h-[55dvh] xl:h-[70dvh] items-center justify-center bg-contain bg-no-repeat"
         style={{
           backgroundImage: `url("../assets/images/clicker-character/verify-email-content-bg.png")`,
           backgroundPosition: "center",
         }}
       >
-        <div className="h-full flex flex-col justify-center items-center mb-4">
+        <div className="h-full flex flex-col justify-center items-center mb-[1rem]">
           <img
-            src={"../assets/images/clicker-character/verify-email-icon.png"}
-            alt="verify email"
-            className="w-auto h-[8dvh] xl:h-[15dvh]"
+            src={"../assets/images/clicker-character/limited-access-icon.png"}
+            alt="limited-access-icon"
+            className="w-auto h-[8dvh] xl:h-[15dvh] mb-[1rem]"
           />
           <h1 className="w-[70%] text-xl md:text-3xl lg:text-5xl font-semibold text-center text-[#ffa900] mb-[0.5rem] lg:mb-[1rem]">
-            verification email has been sent to your gmail
+            can only access this page after email verification
           </h1>
           <p className="w-[90%] text-[#3C3C3C] text-sm xl:text-xl text-center font-normal font-outfit ">
             If you have already verified your email,{" "}
