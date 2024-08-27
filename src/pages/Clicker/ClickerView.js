5b1d8472702e0684d7c3cdb11d67db5ebd4cf6e3
Jeryxem <jeryxem@gmail.com>
2024-08-27 19:06:40 +0800

    Update: Daily Reward Popup Visual

--- src/pages/Clicker/ClickerView.js
+++ src/pages/Clicker/ClickerView.js
@@ -89,35 +89,46 @@ const ClickerView = () => {
           open={isOpenDailyPopup}
           className="h-screen w-screen flex flex-1 overflow-x-hidden overflow-y-auto"
         >
-          <div className="flex flex-1 px-4 py-4 justify-center items-center h-full w-full">
-            <div className="bg-black max-w-80 flex flex-1 relative rounded-lg shadow-lg h-fit">
+          <div className="fixed inset-0 bg-transparent backdrop-blur-xl rounded-xl flex justify-center items-center z-[200] overflow-hidden">
+            <div className="min-h-[800px] md:min-h-[unset] daily-reward-bg relative rounded-xl w-[100%] lg:w-[90%] lg:max-w-[800px] bg-no-repeat bg-cover md:bg-contain"
+              style={{
+                backgroundImage: `url("/assets/images/clicker-character/upgrades-details-bg.png")`,
+                backgroundPosition: "center",
+              }}
+            >
+              {/* Close Button */}
               <button
-                className="absolute right-2 top-2 sm:-right-3 sm:-top-3 bg-pink-500 rounded-full p-2.5 z-50"
+                className="absolute top-[4rem] md:top-[12rem] lg:top-[8rem] right-[5rem] text-white text-4xl hover:brightness-75"
                 onClick={handleClose}
               >
-                <img className="w-3 h-3" src="assets/icons/x.svg" alt="Close" />
+                &times;
               </button>
-              <div className="flex flex-col items-center p-6 sm:p-8">
-                <Box className="pb-5">
-                  <img className="w-20 h-20" src="assets/images/activeDog.png" alt="Active Dog" />
+
+              {/* Reward Content */}
+              <div className="flex flex-col items-center 
+                px-[2rem] pt-[4rem] pb-[6rem] 
+                md:px-[4rem] md:pt-[13rem] md:pb-[14rem] 
+                lg:pt-[8rem] lg:pb-[10rem]">
+                <Box>
+                  <img className="max-w-[8rem] lg:max-w-[10rem]" src="assets/images/DailyRewards1.webp" alt="Daily Rewards" />
                 </Box>
-                <div className="space-y-1 flex flex-col items-center">
-                  <p className="text-white text-4xl font-base text-center">Daily reward</p>
-                  <span className="text-white text-sm font-extralight text-center">
-                    Accrue coins for logging into the game daily without skipping
+                <div className="space-y-1 flex flex-col items-center pb-3">
+                  <p className="text-[#FFAA00] text-[3rem] lg:text-[3.75rem] font-base text-center">Daily reward</p>
+                  <span className="text-white font-outfit text-center font-">
+                    Accure Coins For Loggin Into The Game Daily Without Skipping
                   </span>
                 </div>
-                <Box className="pt-8 pb-4 w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2">
+                <Box className="max-h-[450px] md:max-h-[150px] overflow-y-scroll py-3 pr-2 w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-2 custom-scrollbar">
                   {dailyLoginRewards.map((dayReward, index) => {
                     const isSelected = index < currentUser?.loginDays;
                     return (
                       <Box
-                        className={`${isSelected ? 'bg-green-500' : 'bg-zinc-500'} rounded-md py-1.5 flex`}
+                        className={`${isSelected ? 'bg-[#FFAA00]' : 'bg-[#3C3C3C]'} rounded-md py-1.5 flex`}
                         key={index}
                       >
-                        <p className="flex flex-1 flex-col items-center justify-center text-xs space-y-1">
-                          <span>Day {index + 1}</span>
-                          <img className="w-5 h-5" src="assets/images/gem2.png" alt="Star" />
+                        <p className={`flex flex-1 flex-col items-center justify-center text-xs space-y-1 ${isSelected ? 'text-white' : 'text-[#C5C5C5]'}`}>
+                          <span className='font-outfit font-normal'>Day {index + 1}</span>
+                          <img className="w-5 h-5" src={isSelected ? 'assets/images/coin.webp' : 'assets/images/coin-disable.webp'} alt="Star" />
                           <span>{dayReward}</span>
                         </p>
                       </Box>
