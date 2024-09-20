312f72cbe5a43ec44d646189a40c0c06fcdb959e
TanHouYin <155421724+TanHouYin@users.noreply.github.com>
2024-09-20 13:40:39 +0800

    chore: update minting floating coin position

--- src/pages/Mint/MintPage.js
+++ src/pages/Mint/MintPage.js
@@ -377,7 +377,7 @@ function MintPage() {
           />
 
           <img
-            className="absolute left-[12%] lg:left-[17%] bottom-[5%] lg:bottom-[10%] transform -translate-x-1/2 -translate-y-1/2 z-[20] opacity-65 w-[60px] lg:w-[120px] pointer-events-none"
+            className="absolute left-[12%] lg:left-[17%] top-[50rem] lg:top-[60rem] transform -translate-x-1/2 -translate-y-1/2 z-[20] opacity-65 w-[60px] lg:w-[120px] pointer-events-none"
             src="/assets/images/coin-5.webp"
             alt=""
           />
