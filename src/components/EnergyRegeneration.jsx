2a77988410a8d27cd457f4e4087099baa60df7c6
y3jiojio <triple.y.yvetteyee@gmail.com>
2024-09-20 10:26:40 +0800

    Depletion reward effect, update video

--- src/components/EnergyRegeneration.jsx
+++ src/components/EnergyRegeneration.jsx
@@ -59,10 +59,7 @@ function EnergyRegeneration({ isLeaderboardOpen, setIsLeaderboardOpen, isOneTime
   return (
     <>
       <div
-        className="absolute flex flex-col xl:grid grid-cols-3 gap-3 justify-center items-center w-full mx-auto pt-8 xl:px-12 top-[8rem] xl:top-60"
-        style={{
-          zIndex: 20,
-        }}
+        className="absolute flex flex-col xl:grid grid-cols-3 gap-3 justify-center items-center w-full mx-auto pt-8 xl:px-12 top-[8rem] xl:top-60 z-[99]"
       >
         {/* explora point display */}
         <div
@@ -130,7 +127,7 @@ function EnergyRegeneration({ isLeaderboardOpen, setIsLeaderboardOpen, isOneTime
             isLeaderboardOpen ? 'block' : 'hidden'
           }`}
           style={{
-            zIndex: 100,
+            zIndex: 99,
           }}
         >
           <LeaderBoardModal
