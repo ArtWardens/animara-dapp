a5916bde2c359b8a815cc5452fb206028ea6a6c7
y3jiojio <triple.y.yvetteyee@gmail.com>
2024-10-17 15:39:57 +0800

    Mascot character stop at level 20, update to match max level is 40

--- src/utils/getImagePath.js
+++ src/utils/getImagePath.js
@@ -2,10 +2,11 @@ const basePath = '/assets/images/clicker-character/ani-character-leveling';
 
 export const getAllImagePaths = (currentUser) => {
   const currentLevel = currentUser?.level || 1;
+  const effectiveLevel = Math.floor((currentLevel - 1) / 2) + 1;
   
   return [
-    `${basePath}/${currentLevel}-initial.webp`,
-    `${basePath}/${currentLevel}-hits-1.webp`,
-    `${basePath}/${currentLevel}-hits-2.webp`,
+    `${basePath}/${effectiveLevel}-initial.webp`,
+    `${basePath}/${effectiveLevel}-hits-1.webp`,
+    `${basePath}/${effectiveLevel}-hits-2.webp`,
   ];
 };
